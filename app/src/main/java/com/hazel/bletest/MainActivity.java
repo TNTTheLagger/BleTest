5382cf45fdc62f26533134147a4bf2f46f29107d
TNTTheLagger <tntcsatornaja@gmail.com>
2025-02-18 20:57:03 +0100

    Some code...

--- app/src/main/java/com/hazel/bletest/MainActivity.java
+++ app/src/main/java/com/hazel/bletest/MainActivity.java
@@ -1,24 +1,90 @@
 package com.hazel.bletest;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Handler;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothSocket bluetoothSocket;
+    private InputStream inputStream;
+    private Handler handler = new Handler();
+    private final UUID SERIAL_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // Standard UUID for serial devices
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        textView = findViewById(R.id.textView);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        connectToBluetoothDevice();
+    }
+
+    private void connectToBluetoothDevice() {
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (pairedDevices.isEmpty()) {
+            Toast.makeText(this, "No paired Bluetooth devices found", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        BluetoothDevice device = pairedDevices.iterator().next(); // Get the first paired device (modify to choose a specific one)
+
+        try {
+            bluetoothSocket = device.createRfcommSocketToServiceRecord(SERIAL_UUID);
+            bluetoothSocket.connect();
+            inputStream = bluetoothSocket.getInputStream();
+
+            new Thread(this::readBluetoothData).start(); // Start reading data in a separate thread
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Connection failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void readBluetoothData() {
+        byte[] buffer = new byte[1024];
+        int bytes;
+
+        while (true) {
+            try {
+                bytes = inputStream.read(buffer);
+                String receivedData = new String(buffer, 0, bytes);
+
+                handler.post(() -> textView.append(receivedData + "\n")); // Update UI
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            if (inputStream != null) inputStream.close();
+            if (bluetoothSocket != null) bluetoothSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
