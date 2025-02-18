ed633cb778d01d5022548e2f2f2f205f8120ac88
TNTTheLagger <tntcsatornaja@gmail.com>
2025-02-18 20:57:56 +0100

    Some more code...

--- app/src/main/java/com/hazel/bletest/MainActivity.java
+++ app/src/main/java/com/hazel/bletest/MainActivity.java
@@ -3,11 +3,14 @@ package com.hazel.bletest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Set;
@@ -38,6 +41,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void connectToBluetoothDevice() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
         if (pairedDevices.isEmpty()) {
             Toast.makeText(this, "No paired Bluetooth devices found", Toast.LENGTH_LONG).show();
