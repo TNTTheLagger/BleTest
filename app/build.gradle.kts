477592dca518ab0ff31c89dad275146e84b2e171
TNTTheLagger <tntcsatornaja@gmail.com>
2025-02-25 20:09:53 +0100

    updated sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.hazel.bletest"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hazel.bletest"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
